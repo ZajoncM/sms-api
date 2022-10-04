e97eafd6daee6ffb9d9bf14df07cc7c345fe03ac
ZajoncM <milosz.zajonc@gmail.com>
2022-10-04 13:12:51 +0200

    fix current user

--- src/auth/gql-auth.guard.ts
+++ src/auth/gql-auth.guard.ts
@@ -1,23 +1,10 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from 'src/utils/public.decorator';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super();
-  }
   getRequest(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) {
-      return true;
-    }
-
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
   }
